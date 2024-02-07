f7cf95ae22b31e1a867004a22f66fd008bf92abc
Samuel Gonzalez <Nomekop65@yahoo.com>
2024-02-06 20:20:57 -0500

    Three basic sliders created for hue, saturation, and lightness

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <div>
+      <input type="range" id="hue" name="hue" min="0" max="360" />
+      <label htmlFor="hue">Hue</label>
+      <input type="range" id="saturation" name="saturation" min="0" max="100" />
+      <label htmlFor="saturation">Saturation</label>
+      <input type="range" id="lightness" name="lightness" min="0" max="100" />
+      <label htmlFor="lightness">Lightness</label>
+    </div>
+  )
 }
